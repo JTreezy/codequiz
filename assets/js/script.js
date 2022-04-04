332c78b6d24d9a376020e0b21db2df490bf56db7
Jonathan Tran <digitalsigna@gmail.com>
2022-04-04 00:08:39 -0700

    added interface flip function between states of quiz intro, quiz prompt, quiz results

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var question1 = {
+    title: "What is my favorite color",
+    options: ["silver","blue","black","white"],
+    correctAnswer: "silver"
+}
+
+function startButton() {
+    var x = document.getElementById("start");
+    var y = document.getElementById("coding-prompt");
+  
+    x.style.display = "none";
+    y.style.display = "block";
+}
